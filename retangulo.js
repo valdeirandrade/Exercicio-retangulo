5c3f5d4c31bca4f442f99be3c3fd712bb185cb2a
valdeir <andrade_mai@yahoo.com.br>
2020-05-25 20:16:18 -0400

    alteracao

---
+++ retangulo.js
@@ -0,0 +1,44 @@
+class Retangulo {
+
+    constructor (altura, largura){
+     console.log("altura:"+altura)
+     console.log("largura:"+largura)
+
+     this altura = altura
+     this largura = largura
+    }
+calcularArea() 
+return this.altura*this.largura
+
+}
+
+class Triangulo {
+
+    constructor(){
+
+        this.lado1 = lado1
+        this.lado2 = lado2
+        this.lado3 = lado3
+        this.base = base
+        this.altura = altura
+    }
+    calcularArea() { 
+    
+    return (this.base*this.altura)/2 
+}
+
+ verificardorTipoT(){
+
+    if (lado1==lado2 && lado2==lado3)}
+    return "Triangulo Equilatero"}
+    
+    else{
+        if (lado1!=lado2 && lado1!=lado3! &&lado2!=lado3){
+            return "Triangulo Escaleno"
+        }else{
+            return "Triangulo Isóceles"
+        }
+         
+ }
+
+}
