a1efc6e4aa3a05ff43d5bb5c94d54cd1e134773e
valdeir <andrade_mai@yahoo.com.br>
2020-05-26 20:13:52 -0400

    finalização

--- retangulo.js
+++ retangulo.js
@@ -1,44 +1,48 @@
 class Retangulo {
 
     constructor (altura, largura){
+          
+     this.altura = altura
+     this.largura = largura
+     
      console.log("altura:"+altura)
      console.log("largura:"+largura)
 
-     this altura = altura
-     this largura = largura
+    
     }
-calcularArea() 
-return this.altura*this.largura
+calcularArea() {
 
+return this.altura*this.largura
 }
+}
+
 
 class Triangulo {
 
-    constructor(){
+    constructor(lado1, lado2, lado3, base, altura){
 
-        this.lado1 = lado1
-        this.lado2 = lado2
-        this.lado3 = lado3
-        this.base = base
-        this.altura = altura
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+        this.base = base;
+        this.altura = altura;
     }
     calcularArea() { 
     
-    return (this.base*this.altura)/2 
+    return (this.base*this.altura) /2 
 }
-
  verificardorTipoT(){
 
-    if (lado1==lado2 && lado2==lado3)}
-    return "Triangulo Equilatero"}
-    
-    else{
-        if (lado1!=lado2 && lado1!=lado3! &&lado2!=lado3){
+   if (this.lado1==this.lado2 && this.lado2==this.lado3)
+    return "Triangulo Equilatero"  
+   
+    else if (this.lado1!=this.lado2 && this.lado1!=this.lado3 && this.lado2!=this.lado3)
             return "Triangulo Escaleno"
-        }else{
+        
+        else
             return "Triangulo Isóceles"
-        }
-         
+                 
+    }
+
  }
 
-}
